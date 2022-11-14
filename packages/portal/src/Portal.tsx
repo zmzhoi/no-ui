ffe4ca5ed902e28005f7be49f5bf7999e73a3c1e
zmzhoi <zmzhoi@gmail.com>
2022-11-14 14:12:57 +0900

    change: @no-ui/portal animate prop 추가

--- packages/portal/src/Portal.tsx
+++ packages/portal/src/Portal.tsx
@@ -1,20 +1,54 @@
 import { setSignature } from '@no-ui/utils';
-import { PropsWithChildren } from 'react';
+import { cloneElement, useRef, ReactElement, useLayoutEffect } from 'react';
 import { createPortal } from 'react-dom';
 
-export interface PortalProps extends PropsWithChildren {
+export interface PortalProps {
   visible: boolean;
   container?: Element | DocumentFragment;
+  animate?: boolean;
+  children: ReactElement;
 }
 
-export function Portal({ visible, container = undefined, children }: PortalProps) {
+export function Portal({ visible, container = undefined, animate = false, children }: PortalProps) {
+  const ref = useRef<HTMLElement>(null);
+
+  useLayoutEffect(() => {
+    if (!visible) {
+      return;
+    }
+
+    if (!animate || !ref.current) {
+      return;
+    }
+
+    ref.current.animate(
+      [
+        {
+          opacity: 0.2,
+        },
+        {
+          opacity: 1,
+        },
+      ],
+      {
+        duration: 350,
+        easing: 'cubic-bezier(0.39, 0.575, 0.565, 1)',
+      },
+    );
+  }, [animate, visible]);
+
   if (!visible) {
     return null;
   }
 
   const _container = container || window.document.body;
 
-  return createPortal(children, _container);
+  return createPortal(
+    cloneElement(children, {
+      ref,
+    }),
+    _container,
+  );
 }
 
 setSignature(Portal, __VERSION__);
