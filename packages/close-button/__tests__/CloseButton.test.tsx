c5592892e1558d3366ef492b670f51cf773e2a06
zmzhoi <zmzhoi@gmail.com>
2022-12-05 10:29:28 +0900

    add(close-button): Add test case.

--- packages/close-button/__tests__/CloseButton.test.tsx
+++ packages/close-button/__tests__/CloseButton.test.tsx
@@ -1,5 +1,18 @@
+import { render } from '@testing-library/react';
+
+import SIZES from '../src/sizes';
+import { CloseButton } from '../src/CloseButton';
+
 describe('CloseButton.tsx', () => {
-  test('always pass', () => {
-    expect(0).toBe(0);
+  test(`CloseButton's size should be correct.`, () => {
+    const { getByRole, rerender } = render(<CloseButton size="sm" />);
+
+    expect(getByRole('button').style.width).toBe(SIZES['sm']);
+
+    rerender(<CloseButton size="md" />);
+    expect(getByRole('button').style.width).toBe(SIZES['md']);
+
+    rerender(<CloseButton size="lg" />);
+    expect(getByRole('button').style.width).toBe(SIZES['lg']);
   });
 });
